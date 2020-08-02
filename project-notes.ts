0f3233291ca4e86718aa0434b6cf46bed3bb9d56
integreator <walterfrontier@gmail.com>
2020-08-01 20:30:08 -0400

    add project notes

---
+++ project-notes.ts
@@ -0,0 +1,6 @@
+/**
+ * Remaining tasks:
+ * 1. add bookmark and favorite button to the post card;
+ * 2. add subscribtion functionality(user following) using kafka;
+ * 3. make user avatar and back pic changable;
+ */
